90a0f6af8f0da0ba8c6e970a9a84e80607d23745
Natanael Arndt <arndtn@gmail.com>
2015-06-23 18:37:51 +0200

    Update index to use new saft namespaces

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $rule->substitutions['Saft\\Rdf\\StatementIteratorFactory'] = new \Dice\Instance
 $rule->substitutions['Saft\\Data\\ParserFactory'] = new \Dice\Instance('Saft\\Addition\\EasyRdf\\Data\\ParserFactoryEasyRdf');
 $rule->substitutions['Saft\\Data\\SerializerFactory'] = new \Dice\Instance('Saft\\Data\\SerializerFactoryImpl');
 $rule->substitutions['Saft\\Sparql\\Query\\QueryFactory'] = new \Dice\Instance('Saft\\Sparql\\Query\\QueryFactoryImpl');
-$rule->substitutions['Saft\\Store\\Result\\ResultFactory'] = new \Dice\Instance('Saft\\Store\\Result\\ResultFactoryImpl');
+$rule->substitutions['Saft\\Sparql\\Result\\ResultFactory'] = new \Dice\Instance('Saft\\Sparql\\Result\\ResultFactoryImpl');
 
 $dice = new \Dice\Dice();
 $dice->addRule('*', $rule);
@@ -52,5 +52,5 @@ $store = $dice->create("Saft\\Addition\\ARC2\\Store\\ARC2", $params);
 /*
  * Create application to handle the REST requests
  */
-$app = $dice->create('Saft\\Skeleton\\RestApplication', [$store]);
+$app = $dice->create('Saft\\Skeleton\\Rest\\Application', [$store]);
 $app->run();
