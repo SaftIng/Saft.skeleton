8dcf7fe721ec8602ed948b5b1abd95301866044a
Konrad Abicht <k.abicht@googlemail.com>
2016-02-29 16:20:13 +0100

    port Dice container implementation to new Dice 2.0
    There are no Dice\Instance and Dice\Rule classes for instance.
    We have to use plain arrays to configure Dice.
    
    Added further configurations for more RDF related classes such
    as Saft\Rdf\Literal.

--- src/Saft/Skeleton/Test/DependencyInjection/ContainerDiceTest.php
+++ src/Saft/Skeleton/Test/DependencyInjection/ContainerDiceTest.php
@@ -2,22 +2,62 @@
 
 namespace Saft\Skeleton\Test\DependencyInjection;
 
+use Saft\Rdf\AnyPatternImpl;
+use Saft\Rdf\BlankNodeImpl;
 use Saft\Rdf\LiteralImpl;
+use Saft\Rdf\NamedNodeImpl;
 use Saft\Skeleton\DependencyInjection\ContainerDice;
 use Saft\Skeleton\Test\TestCase;
 
 class ContainerDiceTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $this->fixture = new ContainerDice();
+    }
+
     /*
      * Tests for createInstanceOf
      */
 
-    public function testCreateInstanceOfBasic()
+    public function testCreateInstanceOfAnyPattern()
     {
-        $fixture = new ContainerDice();
+        $this->fixture->setup();
+
+        $this->assertEquals(
+            new AnyPatternImpl(),
+            $this->fixture->createInstanceOf('Saft\Rdf\AnyPattern', array())
+        );
+    }
+
+    public function testCreateInstanceOfBlankNode()
+    {
+        $this->fixture->setup();
+
+        $this->assertEquals(
+            new BlankNodeImpl('_:a'),
+            $this->fixture->createInstanceOf('Saft\Rdf\BlankNode', array('_:a'))
+        );
+    }
+
+    public function testCreateInstanceOfLiteral()
+    {
+        $this->fixture->setup();
+
         $this->assertEquals(
             new LiteralImpl('foo'),
-            $fixture->createInstanceOf('Saft\Rdf\LiteralImpl', array('foo'))
+            $this->fixture->createInstanceOf('Saft\Rdf\Literal', array('foo'))
+        );
+    }
+
+    public function testCreateInstanceOfNamedNode()
+    {
+        $this->fixture->setup();
+
+        $this->assertEquals(
+            new NamedNodeImpl('http://a'),
+            $this->fixture->createInstanceOf('Saft\Rdf\NamedNode', array('http://a'))
         );
     }
 
@@ -27,9 +67,9 @@ class ContainerDiceTest extends TestCase
             $this->markTestSkipped('Array virtuosoConfig is not set in the test-config.yml.');
         }
 
-        $fixture = new ContainerDice();
+        $this->fixture->setup();
 
-        $virtuoso = $fixture->createInstanceOf(
+        $virtuoso = $this->fixture->createInstanceOf(
             'Saft\Addition\Virtuoso\Store\Virtuoso',
             array($this->config['virtuosoConfig'])
         );
