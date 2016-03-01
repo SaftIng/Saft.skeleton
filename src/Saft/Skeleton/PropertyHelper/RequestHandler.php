57199ba204f0eb856d10f49746181fe353b15d42
Konrad Abicht <k.abicht@googlemail.com>
2016-03-01 11:25:49 +0100

    added RequestHandler: encapsulates request handling (createindex + fetchtitles)
    Using RequestHandler makes it easy to deploy a PropertyHelper
    installation, because it hides all the hassle you need to setup
    and init the cache, store, ... .

---
+++ src/Saft/Skeleton/PropertyHelper/RequestHandler.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Saft\Skeleton\PropertyHelper;
+
+use Nette\Caching\Cache;
+use Nette\Caching\Storages\MemoryStorage;
+use Saft\Rdf\NamedNode;
+use Saft\Rdf\NamedNodeImpl;
+use Saft\Store\Store;
+
+/**
+ * Encapsulates PropertyHelper related classes, ensures correct usage and helps users that way
+ * to use this stuff properly.
+ */
+class RequestHandler
+{
+    /**
+     * @var Cache
+     */
+    protected $cache;
+
+    /**
+     * @var NamedNode
+     */
+    protected $graph;
+
+    /**
+     * @var AbstractIndex
+     */
+    protected $index;
+
+    /**
+     * @var IStorage
+     */
+    protected $storage;
+
+    /**
+     * @param Store $store
+     * @param NamedNode $graph Instance of the graph, whose ressources will be collected for the index
+     */
+    public function __construct(Store $store, NamedNode $graph)
+    {
+        $this->graph = $graph;
+        $this->store = $store;
+    }
+
+    /**
+     * @return array Array of string containing available property types.
+     */
+    public function getAvailableTypes()
+    {
+        return array(
+            'title'
+        );
+    }
+
+    /**
+     * @return Cache
+     */
+    public function getCache()
+    {
+        return $this->cache;
+    }
+
+    /**
+     * @param string $action
+     * @param array $payload Neccessary configuration to execute the requested action.
+     */
+    public function handle($action, $payload = array())
+    {
+        if (null == $this->index) {
+            throw new \Exception('Please call setType before handle to initialize the index.');
+        }
+
+        $action = strtolower($action);
+
+        /*
+         * create index for all resources of a graph
+         */
+        if ('createindex' == $action) {
+            return $this->index->createIndex();
+
+        } elseif('fetchtitles' == $action) {
+            return $this->index->fetchTitles($payload);
+        }
+
+        throw new \Exception('Unknown $action given: '. $action);
+    }
+
+    /**
+     * Initializes the cache backend and storage.
+     *
+     * @param array $configuration
+     * @throws \Exception if parameter $configuration is empty
+     * @throws \Exception if parameter $configuration does not have key "name" set
+     * @throws \Exception if an unknown name was given.
+     */
+    public function setupCache(array $configuration)
+    {
+        if (0 == count(array_keys($configuration))) {
+            throw new \Exception('Parameter $configuration must not be empty.');
+        } elseif (false === isset($configuration['name'])) {
+            throw new \Exception('Parameter $configuration does not have key "name" set.');
+        }
+
+        switch($configuration['name']) {
+            // memory storage: lasts as long as the current PHP session is executed.
+            case 'memory':
+                $this->storage = new MemoryStorage();
+                break;
+
+            default:
+                throw new \Exception('Unknown name given: '. $configuration['name']);
+        }
+
+        $this->cache = new Cache($this->storage);
+    }
+
+    /**
+     * Based on given type, according index will be setup.
+     *
+     * @param string $type Type of the property, e.g. title. Check getAvailableTypes for more information.
+     * @throws \Exception if unknown type was given.
+     */
+    public function setType($type)
+    {
+        if (null == $this->cache) {
+            throw new \Exception('Please call setupCache before setType to initialize the cache environment.');
+        }
+
+        // type recognized
+        if (in_array($type, $this->getAvailableTypes())) {
+            switch($type) {
+                case 'title':
+                    $this->index = new TitleHelperIndex($this->cache, $this->store, $this->graph);
+                    return;
+            }
+        }
+
+        throw new \Exception('Unknown type given: '. $type);
+    }
+}
---
+++ src/Saft/Skeleton/Test/PropertyHelper/RequestHandlerTest.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace Saft\Skeleton\Test\PropertyHelper;
+
+use Nette\Caching\Cache;
+use Nette\Caching\Storages\MemoryStorage;
+use Saft\Rdf\LiteralImpl;
+use Saft\Rdf\NamedNodeImpl;
+use Saft\Rdf\NodeFactoryImpl;
+use Saft\Rdf\StatementFactoryImpl;
+use Saft\Rdf\StatementImpl;
+use Saft\Rdf\StatementIteratorFactoryImpl;
+use Saft\Skeleton\PropertyHelper\RequestHandler;
+use Saft\Skeleton\Test\TestCase;
+use Saft\Sparql\Query\QueryFactoryImpl;
+use Saft\Store\BasicTriplePatternStore;
+
+class RequestHandlerTest extends TestCase
+{
+    protected $cache;
+    protected $storage;
+    protected $store;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // cache environment
+        $this->storage = new MemoryStorage();
+        $this->cache = new Cache($this->storage);
+
+        // store
+        $this->store = new BasicTriplePatternStore(
+            new NodeFactoryImpl(),
+            new StatementFactoryImpl(),
+            new QueryFactoryImpl(),
+            new StatementIteratorFactoryImpl()
+        );
+
+        $this->fixture = new RequestHandler($this->store, $this->testGraph);
+    }
+
+    public function fillStoreWithTestData()
+    {
+        // add test data to store
+        $this->store->addStatements(
+            array(
+                new StatementImpl(
+                    new NamedNodeImpl('http://saft/test/s1'),
+                    new NamedNodeImpl('http://purl.org/dc/terms/title'),
+                    new LiteralImpl('s1 dcterms title')
+                ),
+                new StatementImpl(
+                    new NamedNodeImpl('http://saft/test/s2'),
+                    new NamedNodeImpl('http://www.w3.org/2000/01/rdf-schema#label'),
+                    new LiteralImpl('s2 rdfs label')
+                ),
+                new StatementImpl(
+                    new NamedNodeImpl('http://saft/test/s2'),
+                    new NamedNodeImpl('http://purl.org/dc/terms/title'),
+                    new LiteralImpl('s2 dcterms title')
+                ),
+                new StatementImpl(
+                    new NamedNodeImpl('http://saft/test/s2'),
+                    new NamedNodeImpl('http://purl.org/dc/terms/title'),
+                    new LiteralImpl('s2 dcterms title - 2')
+                )
+            ),
+            $this->testGraph
+        );
+    }
+
+    /*
+     * Tests for getAvailableTypes
+     */
+
+    public function testGetAvailableTypes()
+    {
+        $this->assertEquals(
+            array('title'),
+            $this->fixture->getAvailableTypes()
+        );
+    }
+
+    /*
+     * Tests for handle
+     */
+
+    public function testHandleActionCreateIndex()
+    {
+        $this->fillStoreWithTestData();
+
+        // setup cache using memory
+        $this->fixture->setupCache(array('name' => 'memory'));
+
+        // set titlehelper
+        $this->fixture->setType('title');
+
+        $this->assertEquals(
+            array(
+                'http://saft/test/s1' => array(
+                    'titles' => array(array('uri' => 'http://purl.org/dc/terms/title', 'title' => 's1 dcterms title'))
+                ),
+                'http://saft/test/s2' => array(
+                    'titles' => array(
+                        array('uri' => 'http://www.w3.org/2000/01/rdf-schema#label', 'title' => 's2 rdfs label'),
+                        array('uri' => 'http://purl.org/dc/terms/title', 'title' => 's2 dcterms title'),
+                        array('uri' => 'http://purl.org/dc/terms/title', 'title' => 's2 dcterms title - 2'),
+                    )
+                )
+            ),
+            $this->fixture->handle('createIndex')
+        );
+    }
+
+    public function testHandleActionCreateIndexEmptyStore()
+    {
+        // setup cache using memory
+        $this->fixture->setupCache(array('name' => 'memory'));
+
+        // set titlehelper
+        $this->fixture->setType('title');
+
+        $this->assertEquals(array(), $this->fixture->handle('createIndex'));
+    }
+
+    public function testHandleActionCreateIndexNoIndexInitialized()
+    {
+        $this->setExpectedException('Exception');
+
+        $this->assertNull($this->fixture->handle('createIndex'));
+    }
+
+    public function testHandleActionFetchTitles()
+    {
+        $this->fillStoreWithTestData();
+
+        // setup cache using memory
+        $this->fixture->setupCache(array('name' => 'memory'));
+
+        // set titlehelper
+        $this->fixture->setType('title');
+
+        $this->assertTrue(0 < count($this->fixture->handle('createIndex')));
+
+        $this->assertEquals(
+            array('http://saft/test/s2' => 's2 rdfs label'),
+            $this->fixture->handle('fetchTitles', array('http://saft/test/s2'))
+        );
+    }
+
+    public function testHandleActionFetchTitlesEmptyPayload()
+    {
+        $this->fillStoreWithTestData();
+
+        // setup cache using memory
+        $this->fixture->setupCache(array('name' => 'memory'));
+
+        // set titlehelper
+        $this->fixture->setType('title');
+
+        $this->assertTrue(0 < count($this->fixture->handle('createIndex')));
+
+        $this->assertEquals(
+            array(),
+            $this->fixture->handle('fetchTitles')
+        );
+    }
+
+    public function testHandleUnknownAction()
+    {
+        $this->setExpectedException('Exception');
+
+        $this->fixture->handle('unknown');
+    }
+
+    /*
+     * Tests for setType
+     */
+
+    public function testSetType()
+    {
+        $this->fixture->setupCache(array('name' => 'memory'));
+
+        $this->assertNull($this->fixture->setType('title'));
+    }
+
+    public function testSetTypeSetupCacheNotCalledBefore()
+    {
+        $this->setExpectedException('Exception');
+
+        $this->fixture->setType('title');
+    }
+
+    public function testSetTypeUnknownType()
+    {
+        $this->setExpectedException('Exception');
+
+        $this->fixture->setType('unknown');
+    }
+
+    /*
+     * Tests for setupCache
+     */
+
+    public function testSetupCacheParameter()
+    {
+        $this->assertNull($this->fixture->setupCache(array('name' => 'memory')));
+    }
+
+    public function testSetupCacheParameterConfigurationIsEmpty()
+    {
+        $this->setExpectedException('Exception');
+
+        $this->fixture->setupCache(array());
+    }
+
+    public function testSetupCacheParameterConfigurationDoesNotHaveKeyNameSet()
+    {
+        $this->setExpectedException('Exception');
+
+        $this->fixture->setupCache(array('foo' => 'bar'));
+    }
+
+    public function testSetupCacheUnknownNameGiven()
+    {
+        $this->setExpectedException('Exception');
+
+        $this->fixture->setupCache(array('name' => 'unknown'));
+    }
+}
